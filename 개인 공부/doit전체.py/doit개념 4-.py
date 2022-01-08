3d49b037858deb3c7f8aa453b7d13de183a92607
Jee Hyuk Choi <akfktl328@naver.com>
2022-01-08 18:19:52 +0900

    10 commit

--- 개인 공부/doit전체.py/doit개념 4-.py
+++ 개인 공부/doit전체.py/doit개념 4-.py
@@ -34,18 +34,81 @@
 
 # result  = add_many(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 # print(result)
+'''응용!'''
+# def add_mul(choice, *args):
+#     if choice == "add":
+#         result = 0
+#         for i in args:
+#             result = result + i
+#     elif choice == "mul":
+#         result = 1
+#         for i in args:
+#             result = result * i
+#     return result
 
-def add_mul(choice, *args):
-    if choice == "add":
-        result = 0
-        for i in args:
-            result = result + i
-    elif choice == "mul":
-        result = 1
-        for i in args:
-            result = result * i
-    return result
+# result = add_mul("add", 1, 2, 3, 4, 5)
+# result2 = add_mul("mul", 1, 2, 3, 4, 5)
+# print(result, result2)
 
-result = add_mul("add", 1, 2, 3, 4, 5)
-result2 = add_mul("mul", 1, 2, 3, 4, 5)
-print(result, result2)
+'''키워드 피라미터'''
+def print_kwargs(**kwargs): #매개변수의 이름 앞에 **를 붙이면 매개변수는 딕셔너리가 되고 
+    print(kwargs)
+print_kwargs(a =1, name = "foo",age = 3) # 모든 key = value 형태의 결괏값이 딕셔너리에 저장
+
+'''파이썬의 결괏값은 항상 하나이다.'''
+def add_and_mul(a, b):
+    return a+b, a*b   #결괏값이 a+b, a*b 두개처럼 보이지만 a+b, a*b를 합쳐 하나의 튜플로 리턴해준다.
+result = add_and_mul(3, 4)
+print(result)  #(7, 12) <-- 이 하나의 튜플 값을 두 개의 결과값처럼 받고 싶다면 아래와 같이 하면 된다.
+
+result1, result2 = add_and_mul(3, 4)
+print(result1, result2) # 결괏값 7과 12
+
+#return문을 두번 쓰더라도 return을 만나면 함수를 빠져나가기 때문에 처음 만난 return 값 만이 함수의 결과값이 된다.
+
+'''return문의 또다른 쓰임새'''
+def say_nick(nick):
+    if nick == "바보":
+        return
+    print(f"나의 별명은 {nick}입니다.")
+say_nick("바보")   # 즉 "바보"라는 값이 들어오면 return문을 통해 함수를 바로 빠져나가게 하는 것 while문의 break처럼 사용가능하다
+
+'''매개변수의 초깃값 미리 설정하기'''
+
+def say_myself(name, old, man = True):  # man을 True로 지정해두면 자덩으로 설정됨! 매개변수 값이 항상 변하는 것이 아니라면 유용한 방법
+    print(f"나의 이름은 {name}입니다.")
+    print(f"저의 나이는 {old}살 입니다.")
+    if man: print("저는 남자입니다.")
+    else: print("저는 여자입니다.")
+
+say_myself("최지혁", 23) # = say_myself("최지혁", 23, True) 다음과 같은 결과를 도출한다.
+say_myself("최지순", 23, False) # False 를 넣어주면 당연히 반대가 출력된다.
+
+'''초깃값을 설정할때 주의할 점!!'''
+'''초깃값을 갖는 매개변수는 항상 뒤데두도록 하자'''
+# 아래와 같은 형식은 오류가 발생함
+#def say_myself(name, man = True, old): old와 man의 위치 변경은 불가능
+
+'''함수에서 선언한 변수의 효력 범위'''
+# a = 1
+# def vartest(a): # 즉 함수안에서 사용된 변수는 함수 밖에는 어떠한 영향도 미치지 않는다. 
+#     a += 1               # 하지만 다음 문단에서 보는 것처럼 함수 밖의 변수를 함수 안에서 사용할 수는 있다. 
+#     return a
+# df = vartest(a)
+# print(a)
+# print(df)
+
+a = 1
+def vartest():
+    global a  #이처럼 global 명령어를 사용하면 함수 밖의 변수를 함수 안으로 가져올 수는 있으나 사용 자제!!
+    a += 1                      # 함수는 독립적으로 존재하는게 좋음
+vartest()
+print(a)
+
+""">>>>>>>>>>>>대망의 lambda(람다)함수!!!!!!!!!!!!!<<<<<<<<<<<<<"""
+'''lambda 함수의 보다 자세한 내용은 개인공부 > lambda.py 파일을 참고 하도록 하자'''
+# def를 사용할 정도로 복잡하지 않거나 사용할 수 없는 꽀예 쓰인다.
+# 1회용 함수 혹은 예약어라고 함
+add = lambda a, b:a+b
+result4 = add(3, 4)
+print(result4)
--- 개인 공부/lambda.py
+++ 개인 공부/lambda.py
@@ -3,8 +3,9 @@
 # add = lambda x, y : x+y
 # print(add(3,4))
 
+'''아래처럼 lambda 자체를 리스트에 집어넣는 것도 가능'''
 # lambdas = [lambda x:x+10, lambda x:x+100]
-# print(lambdas[0](5))
+# print(lambdas[0](5)) # 0번째 요소(람다 함수)에 5를 대입해라
 # print(lambdas[1](4))
 
 # def plus(a, b): print(a+b)
@@ -12,19 +13,19 @@
 # t = plus
 # t = (20, 50)
 
-# fruit = {'apple':5, 'grape': 10, 'banana': 7, 'peach':3, 'melon':2}
+fruit = {'apple':5, 'grape': 10, 'banana': 7, 'peach':3, 'melon':2}
 
-# def f1(x):           # 이런 함수를 간단하게 람다로 지정이 가능하다.
-#     return x[1]
+def f1(x):           # 이런 함수를 간단하게 람다로 지정이 가능하다.
+    return x[1]
 
-# sorted1 = sorted(fruit.items(), key = f1)   #key다음에는 반드시 함수를 입력 받아야함 #따라서 17번 행 처럼 함수 식을 따로 만들어줘서 입력 가능
-# print(sorted1)
+sorted1 = sorted(fruit.items(), key = f1)   #key다음에는 반드시 함수를 입력 받아야함 #따라서 17번 행 처럼 함수 식을 따로 만들어줘서 입력 가능
+print(sorted1)
 
-# sorted1 = sorted(fruit.items(), key=lambda x:x[0])    #람다를 활용하면 함수를 따로 지정하지 않고 정렬가능하다.
-# sorted2 = sorted(fruit.items(), key=lambda x:x[1])    #람다를 사용하면 코드가 간편해짐!
+sorted1 = sorted(fruit.items(), key=lambda x:x[0])    #람다를 활용하면 함수를 따로 지정하지 않고 정렬가능하다.
+sorted2 = sorted(fruit.items(), key=lambda x:x[1])    #람다를 사용하면 코드가 간편해짐!
 
-# print(sorted1)
-# print(sorted2)
+print(sorted1)
+print(sorted2)
 
 """key가 없어도 람다 사용이 가능하다. 람다와 조건부 표현식 사용법"""
 '''lambda 매개변수들(요소): 식1(최종적으로 하고자하는 식)(이러해라) if 조건식(이렇다면) else 식2, 리스트'''
@@ -32,14 +33,14 @@
 # list(map(lambda x: str(x) if x % 3 == 0 else x, a))
 
 """람다에서 조건부 사용시 주의할점!!"""
-# 람다 표현식에서 if, else를 사용할 때는 :(콜론)을 붙이지 않음
-# if, else와 분법이 다르므로 주의해야함
-# 조건부 표현식은 식1 if 조건식 else 식2 형식으로 사용하며 식1은 조건식이 참일때 식2는 조건식이 거짓일때 시용할 식임
-# 특히 람다 표현식에서는 if를 사용했다면 반드시 else를 사용해야함.
-# if만 사용하면 SyntaxError: invaild syntax 애러 발생
-# 람다 표현식에서는 elif 사용불가
-# 조건이 많을 때는 if와 else를 번갈아가면서 써야함
-# 식1 if 조건식1 else 식2 if 조건식2 else 식3 형식 밑이 예제 식
+'''람다 표현식에서 if, else를 사용할 때는 :(콜론)을 붙이지 않음'''
+'''if, else와 분법이 다르므로 주의해야함'''
+'''조건부 표현식은 식1 if 조건식 else 식2 형식으로 사용하며 식1은 조건식이 참일때 식2는 조건식이 거짓일때 시용할 식임'''
+'''특히 람다 표현식에서는 if를 사용했다면 반드시 else를 사용해야함.'''
+'''if만 사용하면 SyntaxError: invaild syntax 애러 발생'''
+'''람다 표현식에서는 elif 사용불가'''
+'''조건이 많을 때는 if와 else를 번갈아가면서 써야함'''
+'''식1 if 조건식1 else 식2 if 조건식2 else 식3 형식 밑이 예제 식'''
 
 # a = [i for i in range(1, 11)]
 # list(map(lambda x: str(x) if x == 1 else float(x) if x == 2 else x + 10, a ))
